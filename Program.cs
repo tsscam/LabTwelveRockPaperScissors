b9cfbfd592940e7477e4f305fa574f5c304098d0
Theresa Skonieczny <tsscamaro@gmail.com>
2017-04-25 23:18:04 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            string Continue;
+            do
+            {
+                Roshambo game = new Roshambo();
+                game.Play();
+
+
+                Console.WriteLine("Continue?  (y/n)");
+                Continue = Console.ReadLine().ToUpper();
+
+                if (Continue == "N")
+                    break;
+
+                if (Continue == "y")
+                    return;
+
+
+            } while (run == true);
+        }
+    }
+}
